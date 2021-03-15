70c320cf52e4ac83466b87cfdbc53d459cd384ef
dangomushi <>
2021-03-15 18:06:49 +0900

    hex_Pass

--- src/run.cpp
+++ src/run.cpp
@@ -1,19 +1,43 @@
 #include "main.hpp"
 
+void VM ( map<string, string> func, string funcname ) {
+    vector<string> vec = split( func[funcname], "3b" );
+    string vdata;
+    for ( int i = 0; i < vec.size(); i++ ) {
+        vdata = vec[i];
+        if ( vdata != "" ) {
+            cout << vdata << endl;
+        }
+    }
+}
+
 int main( int argc, char **arg ){
     
     ifstream fin( split( arg[1], "." )[0], ios::in | ios::binary );
-    string data;
-    string funcname;
+    string funcname, data;
     vector<string> vec;
     map<string, string> func;
     int hex;
 
     char buf[16];
     while(!fin.eof()) {
-        fin.read(buf,sizeof(1));
-        //print(buf);
-        cout << buf << endl;
+        fin.read(buf,sizeof(16));
+        string data2( buf, 4 );
+        data += data2;
     }
+    vec = split( data, "3b" );
+    for ( int i = 0; i < vec.size(); i++ ) {
+        vec[i] = vec[i] + "3b";
+        if ( vec[i].find("3a") != string::npos ) {
+            funcname = split( vec[i], "3a" )[0];
+            func[funcname] += split( vec[i], "3a" )[1];
+        }
+        else {
+            func[funcname] += vec[i];
+        }
+    }
+    VM( func, funcname );
     return 0;
 }
+
+//mode>int
